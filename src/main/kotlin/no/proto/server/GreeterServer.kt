f9bd5e9b2ee9a3859c05e58b70bc4c93d5c92ad3
Torstein Jensen <torstein.jensen@yahoo.no>
2025-03-15 14:35:59 +0100

    server ferdig

--- src/main/kotlin/no/proto/server/GreeterServer.kt
+++ src/main/kotlin/no/proto/server/GreeterServer.kt
@@ -1,39 +1,37 @@
 package no.proto.server
 
-import io.grpc.Grpc
-import io.grpc.InsecureServerCredentials
-import io.grpc.Server
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
+import io.grpc.ServerBuilder
 
-class GreeterServer {
+class GreeterServer(private val port: Int) {
 
-    private lateinit var server: Server
+    val server = ServerBuilder.forPort(port)
+        .addService(GreeterServiceImpl())
+        .build()
 
-    private fun start(port: Int) {
-        val executor = Executors.newFixedThreadPool(2)
-        server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
-            .executor(executor)
-            .addService(GreeterServiceImpl())
-            .build()
-            .start()
-
-
-
+    fun start() {
+        server.start()
+        println("Server started, listening on $port")
+        Runtime.getRuntime().addShutdownHook(
+            Thread {
+                println("Shutting down gRPC server since JVM is shutting down")
+                this@GreeterServer.stop()
+                println("Server shut down")
+            },
+        )
     }
 
     private fun stop() {
-        server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+        server.shutdown()
     }
 
-    private fun blockUntilShutdown() {
+    fun blockUntilShutdown() {
         server.awaitTermination()
     }
 
-    fun main(args: Array<String>) {
-        val server = GreeterServer()
-        server.start(50051)
-        server.blockUntilShutdown()
-    }
+}
 
+fun main() {
+    val server = GreeterServer(8080)
+    server.start()
+    server.blockUntilShutdown()
 }
--- src/main/kotlin/no/proto/server/GreeterServiceImpl.kt
+++ src/main/kotlin/no/proto/server/GreeterServiceImpl.kt
@@ -1,18 +1,17 @@
 package no.proto.server
 
-import io.grpc.stub.StreamObserver
-import no.proto.generated.GreeterServiceGrpc
+import no.proto.generated.GreeterServiceGrpcKt
 import no.proto.generated.HelloRequest
-import no.proto.generated.HelloResponse
+import no.proto.generated.helloResponse
 
-class GreeterServiceImpl : GreeterServiceGrpc.GreeterServiceImplBase() {
+class GreeterServiceImpl : GreeterServiceGrpcKt.GreeterServiceCoroutineImplBase() {
 
-    //TODO skriv om til kotlin
-    override fun sayHello(request: HelloRequest, responseObserver: StreamObserver<HelloResponse>) {
-        val response = HelloResponse.newBuilder().setMessage("Hello ${request.name}").build()
-        responseObserver.onNext(response)
-        responseObserver.onCompleted()
+    override suspend fun sayHello(request: HelloRequest) =
+        helloResponse {
+            message = "Hello ${request.name}"
+        }
 
-    }
 
 }
+
+
