97f6164008c0c60e4dad4cec609a1c51021869ed
Torstein Jensen <torstein.jensen@yahoo.no>
2025-03-16 11:23:24 +0100

    complete demo setup
    * lagt til proto-fil og begynt config
    
    * mer config og protoc kotlin
    
    * server ferdig
    
    * klient ferdig
    
    * refactor
    
    * oppdatert readme

---
+++ src/main/kotlin/no/proto/poc/GreeterServiceImpl.kt
@@ -0,0 +1,17 @@
+package no.proto.poc
+
+import no.proto.generated.GreeterServiceGrpcKt
+import no.proto.generated.HelloRequest
+import no.proto.generated.helloResponse
+
+class GreeterServiceImpl : GreeterServiceGrpcKt.GreeterServiceCoroutineImplBase() {
+
+    override suspend fun sayHello(request: HelloRequest) =
+        helloResponse {
+            message = "Hello ${request.name}"
+        }
+
+
+}
+
+
